2adeb8eb2df5a4232970679bec5cf5120f863b1a
AdamEXu <contact@adamxu.net>
2024-06-20 08:43:24 -0700

    jjhjn

--- main.py
+++ main.py
@@ -12,38 +12,21 @@ def embedding(text):
     vectors = model(**inputs)
   return vectors.last_hidden_state.mean(dim=1).numpy()
 
-# data_code = [
-#   ['print("")', embedding('print("#i like to comment!!!")').tolist()],
-#   ['n = int(input("pls enter a number."))', embedding('n = int(input("pls enter a number."))').tolist()],
-#   ['x = 5', embedding('x = 5').tolist()]
-# ]
-
-# data_documentation = [
-#   ['Prints a new line', embedding('Prints a new line').tolist()],
-#   ['Takes input from user', embedding('Takes input from user').tolist()],
-#   ['Assigns 5 to x', embedding('Assigns 5 to x').tolist()]
-# ]
-
-# data_explanations = [
-#   ['This is an empty print statement, which will result in a new line.', embedding('This is an empty print statement, which will result in a new line.').tolist()],
-#   ['This code takes an integer input from the user and assigns it to n.', embedding('This code takes an integer input from the user and assigns it to n.').tolist()],
-#   ['This code assigns the value 5 to x.', embedding('This code assigns the value 5 to x.').tolist()]
-# ]
-
-# data_ghcommits = [
-#   ['Added a print statement for better readability of output.', embedding('Added a print statement for better readability of output.').tolist()],
-#   ['Added an input statement so the user may specify the value of int n.', embedding('Added an input statement so the user may specify the value of int n.').tolist()],
-#   ['Changed the value of x to 5.', embedding('Changed the value of x to 5.').tolist()]
-# ]
-
 client = Client()
 
 mycode = "print('Hello, World!')"
 
 script = """
-?[thinga, thingb] <- [['thing1', 'thing2'], ['thing3', 'thing4']];
-"""
+{?[a, b, c] <- [['Acme', 'Sales', '123 Foobar Ave']]
 
+:create dept_info {
+  company_name = a,
+  department_name = b,
+  =>
+  head_count = c,
+}}
+"""
+  
 try:
   res = client.run(script)
   print(res)
